fde57dc11527e3eabb4b632acb41abe8ee4b8796
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-04-09 13:04:46 +0530

    implimenting Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import {Routes, Route} from 'react-router-dom';
-import {Navbar, Home, CreatePost, PostDetail} from './';
+// import {Navbar, Home, CreatePost, PostDetail} from './';
+import Navbar from './Navbar';
+import Home from './Home';
+import PostDetail from './PostDetail';
+import CreatePost from './CreatePost';
+
 function App() {
   return (
     <div className="container">
+      <Navbar/>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/post/:postId" element={<PostDetail/>}/>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './components/App';
 import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
-     <App />
-    </Router> 
-  </React.StrictMode>,
-  document.getElementById('root')
+      <App />
+    </Router>
+  </React.StrictMode>
 );
-
