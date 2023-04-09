2a2ba10b4a049668a54ac8235becee90cd809402
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-04-09 13:12:43 +0530

    Create a post detail

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { firestore } from '../firebase';
+
 function PostDetail() {
-    return (
-      <div className="">
-       
-      </div>
-    );
-  }
-  
-  export default PostDetail;
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .doc(postId)
+      .get()
+      .then((snapshot) => {
+        console.log('snapshot', snapshot.data());
+        setPost(snapshot.data());
+      });
+  }, []);
+
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
+
+export default PostDetail;
